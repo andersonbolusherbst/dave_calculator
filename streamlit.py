ac8f1e2ff65dbc3d5d26cbe3c97a5bc0e83de365
andersondbk <32954765+andersondbk@users.noreply.github.com>
2022-06-15 06:33:15 +0700

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -143,8 +143,11 @@ data = load_data(currency_selector)
 data_load_state = st.text("Loading data... complete!")
 
 st.write(data)
+st.write(data.info())
+st.write(data.type())
 close = data['Close']
 st.write(close)
+
 
 
 
