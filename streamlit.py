86ef800dd7f4f11e00d65f89e28a7b2a14f0f19a
andersondbk <32954765+andersondbk@users.noreply.github.com>
2022-06-14 14:01:48 +0700

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -34,6 +34,7 @@ if pressed:
     calculate(years,rate,escalation,deposit,monthly,m)
     st.balloons()
     final_data = pd.DataFrame(amounts,year_string)
-    st.write(" If you invest "monthly", "m" times a year with an annual escalatin of "escalation", your investment with generate" amounts[-1] "in "years" years")
+    st.write(f" If you invest {monthly}, {m} times a year with an annual escalatin of {escalation}, your investment with generate {amounts[-1]} in {years} years")
     st.dataframe(final_data)
     st.bar_chart(amounts)
+    
