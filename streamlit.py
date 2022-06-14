5fbca8028db1bf36ec6631fa52d9ca52a80d55ca
Bradley M Bolus <80377120+bradbolus@users.noreply.github.com>
2022-06-14 15:30:54 +0700

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -35,7 +35,6 @@ if pressed:
     st.balloons()
     
     final_data = pd.DataFrame(amounts,year_string)
-    st.write("hiya") 
     st.write(f" If you invest {monthly}, {m} times a year with an annual escalation of {escalation}, your investment will generate {amounts[-1]} in {years} years")
     st.dataframe(final_data)
     st.bar_chart(amounts)
