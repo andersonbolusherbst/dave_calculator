2a094954f8cd4b2a734b02af046c9fae0818cc5a
Bradley M Bolus <80377120+bradbolus@users.noreply.github.com>
2022-06-15 14:52:58 +0700

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -223,8 +223,8 @@ if pressed:
 
     fig.figimage(im, xo = 90, yo = 690, zorder=2, alpha=1)
 
-    plotdata.plot(kind='bar', color='lightblue')
-    plotdata.plot(kind='line', color='blue', ms=10)
+    ax.bar(plotdata.index, color='lightblue')
+    ax.line(plotdata["amounts"], color='blue', ms=10)
 
     st.pyplot(fig,ax)
     
