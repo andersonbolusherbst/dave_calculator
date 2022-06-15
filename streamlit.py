e64dcc312f48abcbf3427c3f571403ac395777c3
Bradley M Bolus <80377120+bradbolus@users.noreply.github.com>
2022-06-15 14:39:18 +0700

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -221,7 +221,7 @@ if pressed:
     fig, ax = plt.subplots()
     plotdata = pd.DataFrame(amounts)
 
-    fig.figimage(im, xo = 70, yo = 710, zorder=2, alpha=1)
+    fig.figimage(im, xo = 90, yo = 690, zorder=2, alpha=1)
 
     plotdata.plot(kind='bar', color='lightblue')
     plotdata.plot(kind='line', color='blue', ms=10)
