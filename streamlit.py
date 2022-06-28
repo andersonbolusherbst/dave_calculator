63a0e3dd83e1b1e6a28e5730478cd436f353f80e
andersondbk <32954765+andersondbk@users.noreply.github.com>
2022-06-28 09:53:49 +0700

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -17,137 +17,7 @@ template = env.get_template("template.html")
 
 st.image("bayswaterlogo.png")
 
-### Dont worry just currency work in progress
-currency_list = {
-    'ALL': 'Albania Lek',
-    'AFN': 'Afghanistan Afghani',
-    'ARS': 'Argentina Peso',
-    'AWG': 'Aruba Guilder',
-    'AUD': 'Australia Dollar',
-    'AZN': 'Azerbaijan New Manat',
-    'BSD': 'Bahamas Dollar',
-    'BBD': 'Barbados Dollar',
-    'BDT': 'Bangladeshi taka',
-    'BYR': 'Belarus Ruble',
-    'BZD': 'Belize Dollar',
-    'BMD': 'Bermuda Dollar',
-    'BOB': 'Bolivia Boliviano',
-    'BAM': 'Bosnia and Herzegovina Convertible Marka',
-    'BWP': 'Botswana Pula',
-    'BGN': 'Bulgaria Lev',
-    'BRL': 'Brazil Real',
-    'BND': 'Brunei Darussalam Dollar',
-    'KHR': 'Cambodia Riel',
-    'CAD': 'Canada Dollar',
-    'KYD': 'Cayman Islands Dollar',
-    'CLP': 'Chile Peso',
-    'CNY': 'China Yuan Renminbi',
-    'COP': 'Colombia Peso',
-    'CRC': 'Costa Rica Colon',
-    'HRK': 'Croatia Kuna',
-    'CUP': 'Cuba Peso',
-    'CZK': 'Czech Republic Koruna',
-    'DKK': 'Denmark Krone',
-    'DOP': 'Dominican Republic Peso',
-    'XCD': 'East Caribbean Dollar',
-    'EGP': 'Egypt Pound',
-    'SVC': 'El Salvador Colon',
-    'EEK': 'Estonia Kroon',
-    'EUR': 'Euro Member Countries',
-    'FKP': 'Falkland Islands (Malvinas) Pound',
-    'FJD': 'Fiji Dollar',
-    'GHC': 'Ghana Cedis',
-    'GIP': 'Gibraltar Pound',
-    'GTQ': 'Guatemala Quetzal',
-    'GGP': 'Guernsey Pound',
-    'GYD': 'Guyana Dollar',
-    'HNL': 'Honduras Lempira',
-    'HKD': 'Hong Kong Dollar',
-    'HUF': 'Hungary Forint',
-    'ISK': 'Iceland Krona',
-    'INR': 'India Rupee',
-    'IDR': 'Indonesia Rupiah',
-    'IRR': 'Iran Rial',
-    'IMP': 'Isle of Man Pound',
-    'ILS': 'Israel Shekel',
-    'JMD': 'Jamaica Dollar',
-    'JPY': 'Japan Yen',
-    'JEP': 'Jersey Pound',
-    'KZT': 'Kazakhstan Tenge',
-    'KPW': 'Korea (North) Won',
-    'KRW': 'Korea (South) Won',
-    'KGS': 'Kyrgyzstan Som',
-    'LAK': 'Laos Kip',
-    'LVL': 'Latvia Lat',
-    'LBP': 'Lebanon Pound',
-    'LRD': 'Liberia Dollar',
-    'LTL': 'Lithuania Litas',
-    'MKD': 'Macedonia Denar',
-    'MYR': 'Malaysia Ringgit',
-    'MUR': 'Mauritius Rupee',
-    'MXN': 'Mexico Peso',
-    'MNT': 'Mongolia Tughrik',
-    'MZN': 'Mozambique Metical',
-    'NAD': 'Namibia Dollar',
-    'NPR': 'Nepal Rupee',
-    'ANG': 'Netherlands Antilles Guilder',
-    'NZD': 'New Zealand Dollar',
-    'NIO': 'Nicaragua Cordoba',
-    'NGN': 'Nigeria Naira',
-    'NOK': 'Norway Krone',
-    'OMR': 'Oman Rial',
-    'PKR': 'Pakistan Rupee',
-    'PAB': 'Panama Balboa',
-    'PYG': 'Paraguay Guarani',
-    'PEN': 'Peru Nuevo Sol',
-    'PHP': 'Philippines Peso',
-    'PLN': 'Poland Zloty',
-    'QAR': 'Qatar Riyal',
-    'RON': 'Romania New Leu',
-    'RUB': 'Russia Ruble',
-    'SHP': 'Saint Helena Pound',
-    'SAR': 'Saudi Arabia Riyal',
-    'RSD': 'Serbia Dinar',
-    'SCR': 'Seychelles Rupee',
-    'SGD': 'Singapore Dollar',
-    'SBD': 'Solomon Islands Dollar',
-    'SOS': 'Somalia Shilling',
-    'ZAR': 'South Africa Rand',
-    'LKR': 'Sri Lanka Rupee',
-    'SEK': 'Sweden Krona',
-    'CHF': 'Switzerland Franc',
-    'SRD': 'Suriname Dollar',
-    'SYP': 'Syria Pound',
-    'TWD': 'Taiwan New Dollar',
-    'THB': 'Thailand Baht',
-    'TTD': 'Trinidad and Tobago Dollar',
-    'TRY': 'Turkey Lira',
-    'TRL': 'Turkey Lira',
-    'TVD': 'Tuvalu Dollar',
-    'UAH': 'Ukraine Hryvna',
-    'GBP': 'United Kingdom Pound',
-    'USD': 'United States Dollar',
-    'UYU': 'Uruguay Peso',
-    'UZS': 'Uzbekistan Som',
-    'VEF': 'Venezuela Bolivar',
-    'VND': 'Viet Nam Dong',
-    'YER': 'Yemen Rial',
-    'ZWD': 'Zimbabwe Dollar'
-}
-currency_list = ['ZAR','AUD' 'CAD', 'GBP', 'HKD', 'JPY', 'USD']
-currency_selector = st.selectbox(
-     'Which currency will you be investing with?',
-      currency_list)
-    
-st.write('You selected:', currency_selector)
 
-### Brad Playground
-
-dollarSymbol = CurrencySymbols.get_symbol('USD')
-euroSymbol = CurrencySymbols.get_symbol('EUR')
-britishPoundSymbol = CurrencySymbols.get_symbol('GBP')
-#randSymbol = CurrencySymbols.get_symbol('ZAR')
-#randSymbol = CurrencySymbols.get_symbol('ZAR')
 
 
 
@@ -241,7 +111,7 @@ if pressed:
     
    
     base_price_unit = currency_selector
-    symbols_price_unit = st.sidebar.selectbox('Select target currency to convert to', currency_list)
+    symbols_price_unit = st.selectbox('Select target currency to convert to', currency_list)
 
     # Retrieving currency data from ratesapi.io
     # https://api.ratesapi.io/api/latest?base=AUD&symbols=AUD
