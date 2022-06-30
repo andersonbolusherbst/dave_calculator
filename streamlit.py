8680207408e50dd73420bc4af97f39788be6ac99
andersondbk <32954765+andersondbk@users.noreply.github.com>
2022-06-30 10:57:58 +0700

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -127,7 +127,7 @@ if pressed:
         st.write(acc_cap)
         st.write(acc_int)
         st.write(amounts_rounded)
-        final_data = pd.DataFrame(data=[acc_cap, acc_int,amounts_rounded],columns=['Capital','Interest','Total'])
+        final_data = pd.DataFrame(list(zip(acc_cap, acc_int,amounts_rounded)),columns=['Capital','Interest','Total'])
         #final_data = final_data.T
         st.dataframe(final_data)
         # Retrieving currency data from ratesapi.io
