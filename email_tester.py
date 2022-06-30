0c04150440fca834db37f498be5ef59112b5bc12
Luke Herbst <67044777+LukeHerbst@users.noreply.github.com>
2022-06-30 12:20:13 +0700

    Update email_tester.py

--- email_tester.py
+++ email_tester.py
@@ -3,10 +3,7 @@ from email.mime.multipart import MIMEMultipart
 import smtplib, ssl  ### need to add these imports tio the main streamlit file and to the requirements
 def send_email(monthly,m,escalation,amounts,years,max_contribution=None,currency_selector=None):
     
-    
-    
-    
-    port = 587  # For SSL
+    port = 465  # For SSL
     smtp_server = "smtp.gmail.com"
     sender_email = "bayswatercalculator@gmail.com"  # Enter your address
     receiver_email = "lukeherbst2@gmail.com"  # Enter receiver address
