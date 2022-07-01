5e5be4ddd59154acc0e18bae021f85499019aaa8
andersondbk <32954765+andersondbk@users.noreply.github.com>
2022-07-01 16:03:58 +0700

    Update formulas.py

--- formulas.py
+++ formulas.py
@@ -4,7 +4,7 @@ def continue_calculation(amounts,accumulated_capital,accumulated_interest,x,tota
     for y in range(remaining_years):
         y+=1 
         new_dep_fv = new_deposit*(1+(rate/m))**(y*m)
-        new_ann_fv = max_contribution*(((1+rate/m)**(1*m)-1)/(rate/m))
+        new_ann_fv = max_contribution*(((1+rate/m)**(y*m)-1)/(rate/m))
         new_total_fv = new_ann_fv + new_dep_fv
         capital = capital + (max_contribution*m)
         interest = new_total_fv - capital
