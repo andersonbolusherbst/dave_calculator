ebadead99e4014fb38dd07559b2d90f73bef6ec9
Luke Herbst <67044777+LukeHerbst@users.noreply.github.com>
2022-07-01 11:29:52 +0700

    Update email_tester.py

--- email_tester.py
+++ email_tester.py
@@ -1,7 +1,7 @@
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 import smtplib, ssl  ### need to add these imports tio the main streamlit file and to the requirements
-def send_email(monthly,m,escalation,amounts,years,max_contribution=None,currency_selector=None):
+def send_email(template,monthly,m,escalation,amounts,years,escalatep,deposit,acc_int,acc_cap,ireturn,converted,df,conv_currency_selector,max_contribution=None,currency_selector=None):
     
     port = 465  # For SSL
     smtp_server = "smtp.gmail.com"
@@ -23,21 +23,8 @@ def send_email(monthly,m,escalation,amounts,years,max_contribution=None,currency
     **{escalation}**, your investment will generate **{amounts[-1]}** **{currency_selector}** in **{years}** years
     visit bayswatercapital.co.za for more info, or phone your main man Danger Dave: 09898731948
     """
-    html = f"""
-    <html>
-        <body>
-            <p>Hi,<br>
-                Thanks for using Bayswater Calculator? <br>
-                If you invest <b>{monthly}{currency_selector}</b>, <b>{m}</b> times a year with an annual escalation of 
-                <b>{escalation}</b>, your investment will generate <b>{amounts[-1]}</b> <b>{currency_selector}</b> in <b>{years}</b> years
-                 <img src="C:/Users/Luke/Documents/GitHub/dave_calculator/dave_calculator/visualization.png" alt="Graph">
-                 visit  <a href="http://www.bayswatercapital.co.za">Bayswatercapital</a> <br>
-                for more info, or phone your main man Danger Dave: 09898731948             
-                
-            </p>
-        </body>
-    </html>
-    """
+    html = template
+    
     # Turn these into plain/html MIMEText objects
     part1 = MIMEText(text, "plain")
     part2 = MIMEText(html, "html")
@@ -54,3 +41,197 @@ def send_email(monthly,m,escalation,amounts,years,max_contribution=None,currency
         print(f"mail sentmonthly = {monthly}")
 #send_email(1,2,3,[4],5,"max contrinution","USD")
 
+template = f"""
+<!DOCTYPE html>
+<html xmlns:v="urn:schemas-microsoft-com:vml" xmlns:o="urn:schemas-microsoft-com:office:office" lang="en">
+
+<head>
+	<title></title>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<!--[if mso]><xml><o:OfficeDocumentSettings><o:PixelsPerInch>96</o:PixelsPerInch><o:AllowPNG/></o:OfficeDocumentSettings></xml><![endif]-->
+	<style>
+		* {{
+			box-sizing: border-box;
+		}}
+
+		body {{
+			margin: 0;
+			padding: 0;
+		}}
+
+		a[x-apple-data-detectors] {{
+			color: inherit !important;
+			text-decoration: inherit !important;
+		}}
+
+		#MessageViewBody a {{
+			color: inherit;
+			text-decoration: none;
+		}}
+
+		p {{
+			line-height: inherit
+		}}
+
+		.desktop_hide,
+		.desktop_hide table {{
+			mso-hide: all;
+			display: none;
+			max-height: 0px;
+			overflow: hidden;
+		}}
+
+		@media (max-width:620px) {{
+			.desktop_hide table.icons-inner {{
+				display: inline-block !important;
+			}}
+
+			.icons-inner {{
+				text-align: center;
+			}}
+
+			.icons-inner td {{
+				margin: 0 auto;
+			}}
+
+			.image_block img.big,
+			.row-content {{
+				width: 100% !important;
+			}}
+
+			.mobile_hide {{
+				display: none;
+			}}
+
+			.stack .column {{
+				width: 100%;
+				display: block;
+			}}
+
+			.mobile_hide {{
+				min-height: 0;
+				max-height: 0;
+				max-width: 0;
+				overflow: hidden;
+				font-size: 0px;
+			}}
+
+			.desktop_hide,
+			.desktop_hide table {{
+				display: table !important;
+				max-height: none !important;
+			}}
+		}}
+	</style>
+</head>
+
+<body style="background-color: #ffffff; margin: 0; padding: 0; -webkit-text-size-adjust: none; text-size-adjust: none;">
+	<table class="nl-container" width="100%" border="0" cellpadding="0" cellspacing="0" role="presentation" style="mso-table-lspace: 0pt; mso-table-rspace: 0pt; background-color: #ffffff;">
+		<tbody>
+			<tr>
+				<td>
+					<table class="row row-1" align="center" width="100%" border="0" cellpadding="0" cellspacing="0" role="presentation" style="mso-table-lspace: 0pt; mso-table-rspace: 0pt;">
+						<tbody>
+							<tr>
+								<td>
+									<table class="row-content stack" align="center" border="0" cellpadding="0" cellspacing="0" role="presentation" style="mso-table-lspace: 0pt; mso-table-rspace: 0pt; background-color: #fffff; color: #000000; width: 600px;" width="600">
+										<tbody>
+											<tr>
+												<td class="column column-1" width="100%" style="mso-table-lspace: 0pt; mso-table-rspace: 0pt; font-weight: 400; text-align: left; vertical-align: top; padding-top: 5px; padding-bottom: 5px; border-top: 0px; border-right: 0px; border-bottom: 0px; border-left: 0px;">
+													<table class="heading_block" width="100%" border="0" cellpadding="0" cellspacing="0" role="presentation" style="mso-table-lspace: 0pt; mso-table-rspace: 0pt;">
+														<tr>
+															<td style="width:100%;text-align:center;">
+																<h1 style="margin: 0; color: #8a3c90; font-size: 38px; font-family: Arial, 'Helvetica Neue', Helvetica, sans-serif; line-height: 120%; text-align: left; direction: ltr; font-weight: 700; letter-spacing: normal; margin-top: 0; margin-bottom: 0;">Thanks for using the bayswater calculator.</h1>
+															</td>
+														</tr>
+													</table>
+													<table class="paragraph_block" width="100%" border="0" cellpadding="0" cellspacing="0" role="presentation" style="mso-table-lspace: 0pt; mso-table-rspace: 0pt; word-break: break-word;">
+														<tr>
+															<td>
+																<div style="color:#101112;direction:ltr;font-family:Arial, 'Helvetica Neue', Helvetica, sans-serif;font-size:16px;font-weight:400;letter-spacing:0px;line-height:120%;text-align:left;mso-line-height-alt:19.2px;">
+																	<p style="margin: 0; margin-bottom: 16px;"><br><br>If you invest {monthly} {currency_selector}, &nbsp;{m} times a year with an annual escalation of {escalatep}, with a desposit of {deposit} {currency_selector} at a growth rate of {growth_rate}, your investment will generate {amounts[-1]}{currency_selector} in {years} years</p>
+																	<p style="margin: 0; margin-bottom: 16px;">The converted value of your investment is: {converted}</p>
+																	<p style="margin: 0; margin-bottom: 16px;">{conv_currency_selector} at a rate of {df['info']['rate']} in {years} years.<br>&nbsp;You will earn earn {acc_int[-1]} {currency_selector} on your capital contribution of {acc_cap[-1]} {currency_selector} which is a return of {ireturn}</p>
+																	<p style="margin: 0; margin-bottom: 16px;">&nbsp;</p>
+																	<p style="margin: 0;">&nbsp;</p>
+																</div>
+															</td>
+														</tr>
+													</table>
+													<table class="addon_block" width="100%" border="0" cellpadding="0" cellspacing="0" role="presentation" style="mso-table-lspace: 0pt; mso-table-rspace: 0pt;">
+														<tr>
+															<td style="width:100%;padding-right:0px;padding-left:0px;">
+																<div align="center" style="line-height:10px"><img src="https://media0.giphy.com/media/LttSisqcfEPz6BUELG/giphy.gif?cid=20eb4e9dzn0pbskhgkpflgv3vfbc0yg64igom7fd19y6yzax&rid=giphy.gif&ct=s" style="display: block; height: auto; width: 199px; max-width: 100%;" width="199" alt="Image" title="Image"></div>
+															</td>
+														</tr>
+													</table>
+													<table class="image_block" width="100%" border="0" cellpadding="0" cellspacing="0" role="presentation" style="mso-table-lspace: 0pt; mso-table-rspace: 0pt;">
+														<tr>
+															<td style="width:100%;padding-right:0px;padding-left:0px;">
+																<div align="center" style="line-height:10px"><img class="big" src="https://d15k2d11r6t6rl.cloudfront.net/public/users/Integrators/BeeProAgency/829359_813346/1.png" style="display: block; height: auto; border: 0; width: 600px; max-width: 100%;" width="600"></div>
+															</td>
+														</tr>
+													</table>
+													<table class="button_block" width="100%" border="0" cellpadding="0" cellspacing="0" role="presentation" style="mso-table-lspace: 0pt; mso-table-rspace: 0pt;">
+														<tr>
+															<td style="text-align:center;">
+																<div align="center">
+																	<!--[if mso]><v:roundrect xmlns:v="urn:schemas-microsoft-com:vml" xmlns:w="urn:schemas-microsoft-com:office:word" href="bayswatercapital.co.za" style="height:42px;width:165px;v-text-anchor:middle;" arcsize="10%" stroke="false" fillcolor="#ddc385"><w:anchorlock/><v:textbox inset="0px,0px,0px,0px"><center style="color:#ffffff; font-family:Arial, sans-serif; font-size:16px"><![endif]--><a href="bayswatercapital.co.za" target="_blank" style="text-decoration:none;display:inline-block;color:#ffffff;background-color:#ddc385;border-radius:4px;width:auto;border-top:1px solid #ddc385;font-weight:400;border-right:1px solid #ddc385;border-bottom:1px solid #ddc385;border-left:1px solid #ddc385;padding-top:5px;padding-bottom:5px;font-family:Arial, 'Helvetica Neue', Helvetica, sans-serif;text-align:center;mso-border-alt:none;word-break:keep-all;"><span style="padding-left:20px;padding-right:20px;font-size:16px;display:inline-block;letter-spacing:normal;"><span style="font-size: 16px; line-height: 2; word-break: break-word; mso-line-height-alt: 32px;">Go to our website</span></span></a>
+																	<!--[if mso]></center></v:textbox></v:roundrect><![endif]-->
+																</div>
+															</td>
+														</tr>
+													</table>
+												</td>
+											</tr>
+										</tbody>
+									</table>
+								</td>
+							</tr>
+						</tbody>
+					</table>
+					<table class="row row-2" align="center" width="100%" border="0" cellpadding="0" cellspacing="0" role="presentation" style="mso-table-lspace: 0pt; mso-table-rspace: 0pt;">
+						<tbody>
+							<tr>
+								<td>
+									<table class="row-content stack" align="center" border="0" cellpadding="0" cellspacing="0" role="presentation" style="mso-table-lspace: 0pt; mso-table-rspace: 0pt; background-color: #fffff; color: #000000; width: 600px;" width="600">
+										<tbody>
+											<tr>
+												<td class="column column-1" width="100%" style="mso-table-lspace: 0pt; mso-table-rspace: 0pt; font-weight: 400; text-align: left; vertical-align: top; padding-top: 5px; padding-bottom: 5px; border-top: 0px; border-right: 0px; border-bottom: 0px; border-left: 0px;">
+													<table class="icons_block" width="100%" border="0" cellpadding="0" cellspacing="0" role="presentation" style="mso-table-lspace: 0pt; mso-table-rspace: 0pt;">
+														<tr>
+															<td style="vertical-align: middle; color: #9d9d9d; font-family: inherit; font-size: 15px; padding-bottom: 5px; padding-top: 5px; text-align: center;">
+																<table width="100%" cellpadding="0" cellspacing="0" role="presentation" style="mso-table-lspace: 0pt; mso-table-rspace: 0pt;">
+																	<tr>
+																		<td style="vertical-align: middle; text-align: center;">
+																			<!--[if vml]><table align="left" cellpadding="0" cellspacing="0" role="presentation" style="display:inline-block;padding-left:0px;padding-right:0px;mso-table-lspace: 0pt;mso-table-rspace: 0pt;"><![endif]-->
+																			<!--[if !vml]><!-->
+																			<table class="icons-inner" style="mso-table-lspace: 0pt; mso-table-rspace: 0pt; display: inline-block; margin-right: -4px; padding-left: 0px; padding-right: 0px;" cellpadding="0" cellspacing="0" role="presentation">
+																				<!--<![endif]-->
+																				<tr>
+																					<td style="vertical-align: middle; text-align: center; padding-top: 5px; padding-bottom: 5px; padding-left: 5px; padding-right: 6px;"><a href="https://www.designedwithbee.com/?utm_source=editor&utm_medium=bee_pro&utm_campaign=free_footer_link" target="_blank" style="text-decoration: none;"><img class="icon" alt="Designed with BEE" src="https://d15k2d11r6t6rl.cloudfront.net/public/users/Integrators/BeeProAgency/53601_510656/Signature/bee.png" height="32" width="34" align="center" style="display: block; height: auto; margin: 0 auto; border: 0;"></a></td>
+																					<td style="font-family: Arial, 'Helvetica Neue', Helvetica, sans-serif; font-size: 15px; color: #9d9d9d; vertical-align: middle; letter-spacing: undefined; text-align: center;"><a href="https://www.designedwithbee.com/?utm_source=editor&utm_medium=bee_pro&utm_campaign=free_footer_link" target="_blank" style="color: #9d9d9d; text-decoration: none;">Designed with BEE</a></td>
+																				</tr>
+																			</table>
+																		</td>
+																	</tr>
+																</table>
+															</td>
+														</tr>
+													</table>
+												</td>
+											</tr>
+										</tbody>
+									</table>
+								</td>
+							</tr>
+						</tbody>
+					</table>
+				</td>
+			</tr>
+		</tbody>
+	</table><!-- End -->
+</body>
+
+</html>"""
