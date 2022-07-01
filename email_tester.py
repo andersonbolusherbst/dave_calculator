e96f192f17cd63ec50d50e070d07531a5e873a5f
Luke Herbst <67044777+LukeHerbst@users.noreply.github.com>
2022-07-01 11:50:25 +0700

    Update email_tester.py

--- email_tester.py
+++ email_tester.py
@@ -1,7 +1,7 @@
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 import smtplib, ssl  ### need to add these imports tio the main streamlit file and to the requirements
-def send_email(monthly,m,escalation,amounts,years,escalatep,deposit,acc_int,acc_cap,ireturn,converted,df,conv_currency_selector,max_contribution=None,currency_selector=None):
+def send_email(monthly,m,escalation,amounts,years,escalatep,deposit,acc_int,acc_cap,ireturn,converted,df,conv_currency_selector,growth_rate,max_contribution=None,currency_selector=None):
     
     port = 465  # For SSL
     smtp_server = "smtp.gmail.com"
