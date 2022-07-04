e6831b6d8e9fa7cb36100256c10956ce6bcb3999
andersondbk <32954765+andersondbk@users.noreply.github.com>
2022-07-04 14:32:40 +0700

    Update email_tester.py

--- email_tester.py
+++ email_tester.py
@@ -26,73 +26,95 @@ def send_email(monthly,m,escalation,amounts,years,escalatep,deposit,acc_int,acc_
     html = f"""
 <!DOCTYPE html>
 <html xmlns:v="urn:schemas-microsoft-com:vml" xmlns:o="urn:schemas-microsoft-com:office:office" lang="en">
+
 <head>
 	<title></title>
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<!--[if mso]><xml><o:OfficeDocumentSettings><o:PixelsPerInch>96</o:PixelsPerInch><o:AllowPNG/></o:OfficeDocumentSettings></xml><![endif]-->
+	<!--[if !mso]><!-->
+	<link href="https://fonts.googleapis.com/css?family=Abril+Fatface" rel="stylesheet" type="text/css">
+	<link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet" type="text/css">
+	<link href="https://fonts.googleapis.com/css?family=Permanent+Marker" rel="stylesheet" type="text/css">
+	<link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet" type="text/css">
+	<link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet" type="text/css">
+	<!--<![endif]-->
 	<style>
-		* {{
+		* {
 			box-sizing: border-box;
-		}}
-		body {{
+		}
+
+		body {
 			margin: 0;
 			padding: 0;
-		}}
-		a[x-apple-data-detectors] {{
+		}
+
+		a[x-apple-data-detectors] {
 			color: inherit !important;
 			text-decoration: inherit !important;
-		}}
-		#MessageViewBody a {{
+		}
+
+		#MessageViewBody a {
 			color: inherit;
 			text-decoration: none;
-		}}
-		p {{
+		}
+
+		p {
 			line-height: inherit
-		}}
+		}
+
 		.desktop_hide,
-		.desktop_hide table {{
+		.desktop_hide table {
 			mso-hide: all;
 			display: none;
 			max-height: 0px;
 			overflow: hidden;
-		}}
-		@media (max-width:620px) {{
-			.desktop_hide table.icons-inner {{
+		}
+
+		@media (max-width:620px) {
+			.desktop_hide table.icons-inner {
 				display: inline-block !important;
-			}}
-			.icons-inner {{
+			}
+
+			.icons-inner {
 				text-align: center;
-			}}
-			.icons-inner td {{
+			}
+
+			.icons-inner td {
 				margin: 0 auto;
-			}}
+			}
+
 			.image_block img.big,
-			.row-content {{
+			.row-content {
 				width: 100% !important;
-			}}
-			.mobile_hide {{
+			}
+
+			.mobile_hide {
 				display: none;
-			}}
-			.stack .column {{
+			}
+
+			.stack .column {
 				width: 100%;
 				display: block;
-			}}
-			.mobile_hide {{
+			}
+
+			.mobile_hide {
 				min-height: 0;
 				max-height: 0;
 				max-width: 0;
 				overflow: hidden;
 				font-size: 0px;
-			}}
+			}
+
 			.desktop_hide,
-			.desktop_hide table {{
+			.desktop_hide table {
 				display: table !important;
 				max-height: none !important;
-			}}
-		}}
+			}
+		}
 	</style>
 </head>
+
 <body style="background-color: #ffffff; margin: 0; padding: 0; -webkit-text-size-adjust: none; text-size-adjust: none;">
 	<table class="nl-container" width="100%" border="0" cellpadding="0" cellspacing="0" role="presentation" style="mso-table-lspace: 0pt; mso-table-rspace: 0pt; background-color: #ffffff;">
 		<tbody>
@@ -108,41 +130,57 @@ def send_email(monthly,m,escalation,amounts,years,escalatep,deposit,acc_int,acc_
 												<td class="column column-1" width="100%" style="mso-table-lspace: 0pt; mso-table-rspace: 0pt; font-weight: 400; text-align: left; vertical-align: top; padding-top: 5px; padding-bottom: 5px; border-top: 0px; border-right: 0px; border-bottom: 0px; border-left: 0px;">
 													<table class="heading_block" width="100%" border="0" cellpadding="0" cellspacing="0" role="presentation" style="mso-table-lspace: 0pt; mso-table-rspace: 0pt;">
 														<tr>
-															<td style="width:100%;text-align:center;">
-																<h1 style="margin: 0; color: #8a3c90; font-size: 38px; font-family: Arial, 'Helvetica Neue', Helvetica, sans-serif; line-height: 120%; text-align: left; direction: ltr; font-weight: 700; letter-spacing: normal; margin-top: 0; margin-bottom: 0;">Thanks for using the bayswater calculator.</h1>
+															<td style="text-align:center;width:100%;">
+																<h1 style="margin: 0; color: #ddc385; direction: ltr; font-family: Arial, 'Helvetica Neue', Helvetica, sans-serif; font-size: 38px; font-weight: 700; letter-spacing: normal; line-height: 120%; text-align: left; margin-top: 0; margin-bottom: 0;"><span class="tinyMce-placeholder">Thanks for using the Bayswater Capital calculator.</span></h1>
 															</td>
 														</tr>
 													</table>
 													<table class="paragraph_block" width="100%" border="0" cellpadding="0" cellspacing="0" role="presentation" style="mso-table-lspace: 0pt; mso-table-rspace: 0pt; word-break: break-word;">
 														<tr>
 															<td>
-																<div style="color:#101112;direction:ltr;font-family:Arial, 'Helvetica Neue', Helvetica, sans-serif;font-size:16px;font-weight:400;letter-spacing:0px;line-height:120%;text-align:left;mso-line-height-alt:19.2px;">
-																	<p style="margin: 0; margin-bottom: 16px;"><br><br>If you invest {monthly} {currency_selector}, &nbsp;{m} times a year with an annual escalation of {escalatep}, with a desposit of {deposit} {currency_selector} at a growth rate of {growth_rate}, your investment will generate {amounts[-1]}{currency_selector} in {years} years</p>
-																	<p style="margin: 0; margin-bottom: 16px;">The converted value of your investment is: {converted}</p>
-																	<p style="margin: 0; margin-bottom: 16px;">{conv_currency_selector} at a rate of {df['info']['rate']} in {years} years.<br>&nbsp;You will earn earn {acc_int[-1]} {currency_selector} on your capital contribution of {acc_cap[-1]} {currency_selector} which is a return of {ireturn}</p>
+																<div style="color:#0d1a34;direction:ltr;font-family:'Montserrat', 'Trebuchet MS', 'Lucida Grande', 'Lucida Sans Unicode', 'Lucida Sans', Tahoma, sans-serif;font-size:16px;font-weight:400;letter-spacing:0px;line-height:120%;text-align:left;mso-line-height-alt:19.2px;">
+																	<p style="margin: 0; margin-bottom: 16px;"><br><br>If you invest **{monthly}** **{currency_selector}**, &nbsp;**{m}** times a year with an annual escalation of **{escalatep}**, with a desposit of **{deposit}** **{currency_selector}** at a growth rate of **{growth_rate}**, your investment will generate **{amounts[-1]}** **{currency_selector}** in **{years}** years</p>
+																	<p style="margin: 0; margin-bottom: 16px;">The converted value of your investment is: **{converted}** **{conv_currency_selector}** at a rate of **{df['info']['rate']}** in **{years}** years.</p>
+																	<p style="margin: 0; margin-bottom: 16px;"><br>&nbsp;You will earn earn **{acc_int[-1]}** **{currency_selector}** on your capital contribution of **{acc_cap[-1]}** **{currency_selector}** which is a return of **{ireturn}**</p>
 																	<p style="margin: 0; margin-bottom: 16px;">&nbsp;</p>
 																	<p style="margin: 0;">&nbsp;</p>
 																</div>
 															</td>
 														</tr>
 													</table>
-													<table class="addon_block" width="100%" border="0" cellpadding="0" cellspacing="0" role="presentation" style="mso-table-lspace: 0pt; mso-table-rspace: 0pt;">
+													<table class="paragraph_block" width="100%" border="0" cellpadding="0" cellspacing="0" role="presentation" style="mso-table-lspace: 0pt; mso-table-rspace: 0pt; word-break: break-word;">
 														<tr>
-															<td style="width:100%;padding-right:0px;padding-left:0px;">
-																<div align="center" style="line-height:10px"><img src="https://media0.giphy.com/media/LttSisqcfEPz6BUELG/giphy.gif?cid=20eb4e9dzn0pbskhgkpflgv3vfbc0yg64igom7fd19y6yzax&rid=giphy.gif&ct=s" style="display: block; height: auto; width: 199px; max-width: 100%;" width="199" alt="Image" title="Image"></div>
+															<td>
+																<div style="color:#0d1a34;direction:ltr;font-family:Arial, 'Helvetica Neue', Helvetica, sans-serif;font-size:16px;font-weight:400;letter-spacing:0px;line-height:120%;text-align:left;mso-line-height-alt:19.2px;">
+																	<p style="margin: 0;">If you are interested in knowing more feel free to contact us via our website.</p>
+																</div>
 															</td>
 														</tr>
 													</table>
 													<table class="image_block" width="100%" border="0" cellpadding="0" cellspacing="0" role="presentation" style="mso-table-lspace: 0pt; mso-table-rspace: 0pt;">
 														<tr>
-															<td style="width:100%;padding-right:0px;padding-left:0px;">
-																<div align="center" style="line-height:10px"><img class="big" src="https://d15k2d11r6t6rl.cloudfront.net/public/users/Integrators/BeeProAgency/829359_813346/1.png" style="display: block; height: auto; border: 0; width: 600px; max-width: 100%;" width="600"></div>
+															<td style="width:100%;padding-right:0px;padding-left:0px;padding-top:60px;">
+																<div align="center" style="line-height:10px"><img class="big" src="https://d15k2d11r6t6rl.cloudfront.net/public/users/Integrators/BeeProAgency/829359_813346/editor_images/42602f94-fe5a-461d-b076-b608353b4fab.png" style="display: block; height: auto; border: 0; width: 600px; max-width: 100%;" width="600"></div>
+															</td>
+														</tr>
+													</table>
+													<table class="social_block" width="100%" border="0" cellpadding="0" cellspacing="0" role="presentation" style="mso-table-lspace: 0pt; mso-table-rspace: 0pt;">
+														<tr>
+															<td style="padding-top:10px;text-align:center;padding-right:0px;padding-left:0px;">
+																<table class="social-table" width="144px" border="0" cellpadding="0" cellspacing="0" role="presentation" align="center" style="mso-table-lspace: 0pt; mso-table-rspace: 0pt;">
+																	<tr>
+																		<td style="padding:0 2px 0 2px;"><a href="https://www.facebook.com/" target="_blank"><img src="https://app-rsrc.getbee.io/public/resources/social-networks-icon-sets/t-only-logo-dark-gray/facebook@2x.png" width="32" height="32" alt="Facebook" title="facebook" style="display: block; height: auto; border: 0;"></a></td>
+																		<td style="padding:0 2px 0 2px;"><a href="https://www.twitter.com/" target="_blank"><img src="https://app-rsrc.getbee.io/public/resources/social-networks-icon-sets/t-only-logo-dark-gray/twitter@2x.png" width="32" height="32" alt="Twitter" title="twitter" style="display: block; height: auto; border: 0;"></a></td>
+																		<td style="padding:0 2px 0 2px;"><a href="https://www.linkedin.com/" target="_blank"><img src="https://app-rsrc.getbee.io/public/resources/social-networks-icon-sets/t-only-logo-dark-gray/linkedin@2x.png" width="32" height="32" alt="Linkedin" title="linkedin" style="display: block; height: auto; border: 0;"></a></td>
+																		<td style="padding:0 2px 0 2px;"><a href="https://www.instagram.com/" target="_blank"><img src="https://app-rsrc.getbee.io/public/resources/social-networks-icon-sets/t-only-logo-dark-gray/instagram@2x.png" width="32" height="32" alt="Instagram" title="instagram" style="display: block; height: auto; border: 0;"></a></td>
+																	</tr>
+																</table>
 															</td>
 														</tr>
 													</table>
 													<table class="button_block" width="100%" border="0" cellpadding="0" cellspacing="0" role="presentation" style="mso-table-lspace: 0pt; mso-table-rspace: 0pt;">
 														<tr>
-															<td style="text-align:center;">
+															<td style="padding-bottom:10px;padding-top:10px;text-align:center;">
 																<div align="center">
 																	<!--[if mso]><v:roundrect xmlns:v="urn:schemas-microsoft-com:vml" xmlns:w="urn:schemas-microsoft-com:office:word" href="bayswatercapital.co.za" style="height:42px;width:165px;v-text-anchor:middle;" arcsize="10%" stroke="false" fillcolor="#ddc385"><w:anchorlock/><v:textbox inset="0px,0px,0px,0px"><center style="color:#ffffff; font-family:Arial, sans-serif; font-size:16px"><![endif]--><a href="bayswatercapital.co.za" target="_blank" style="text-decoration:none;display:inline-block;color:#ffffff;background-color:#ddc385;border-radius:4px;width:auto;border-top:1px solid #ddc385;font-weight:400;border-right:1px solid #ddc385;border-bottom:1px solid #ddc385;border-left:1px solid #ddc385;padding-top:5px;padding-bottom:5px;font-family:Arial, 'Helvetica Neue', Helvetica, sans-serif;text-align:center;mso-border-alt:none;word-break:keep-all;"><span style="padding-left:20px;padding-right:20px;font-size:16px;display:inline-block;letter-spacing:normal;"><span style="font-size: 16px; line-height: 2; word-break: break-word; mso-line-height-alt: 32px;">Go to our website</span></span></a>
 																	<!--[if mso]></center></v:textbox></v:roundrect><![endif]-->
@@ -200,6 +238,7 @@ def send_email(monthly,m,escalation,amounts,years,escalatep,deposit,acc_int,acc_
 		</tbody>
 	</table><!-- End -->
 </body>
+
 </html>"""
     
     # Turn these into plain/html MIMEText objects
