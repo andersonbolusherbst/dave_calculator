eaf1c17d1d1c8058b850228c6bbb5a7f86d3bb65
Luke Herbst <67044777+LukeHerbst@users.noreply.github.com>
2022-09-12 12:13:51 +0700

    Update streamlitclean.py

--- streamlitclean.py
+++ streamlitclean.py
@@ -259,7 +259,7 @@ if pressed:
 
         st.header('Your Investment Value')
         st.write(f" If you desposit **R{deposit}** and contribute **R{monthly}**,  **{m}** times a year with an annual escalation of **{escalatep}**,  at a growth rate of **{display_rate}%**, your investment will generate **R{final_amount}**  in **{years}** years.")
-        st.write(f"Taking an expected inflation rate of **{inflation}** into account, your real investment value will be **{final_inf_adjusted_return}**")
+        st.write(f"Taking an expected inflation rate of **{inflation}%** into account, your real investment value will be **R{final_inf_adjusted_return}**")
         st.write(f"The converted value of your investment is:  **{conv_currency_selector}** **{converted}** at a rate of **{df['info']['rate']}** ")
         st.write(f" You will earn earn **R{final_interest}**  on your capital contribution of **R{final_cap}**  which is a return of **{ireturn}**")
 
